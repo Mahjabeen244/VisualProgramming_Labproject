8858666efb7ba6d7e6efbec6a0e79f7632e31eab
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 10:15:53 +0500

    added ppadding to the left and side bars

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb58c623989801dc1e8a7da35d879e2ca558907a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3e53f29145318d25dfc98b16bfbcf790f0d428")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
