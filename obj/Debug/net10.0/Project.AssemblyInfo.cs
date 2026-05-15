fb58c623989801dc1e8a7da35d879e2ca558907a
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 10:06:02 +0500

    added hero section

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486d1b6a4af868643220413f8ddfe8449c44815c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1fb955f3962c20abcc16ff491c5d4b2a95c81af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
