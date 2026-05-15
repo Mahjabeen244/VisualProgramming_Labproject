ee53cf0d1ec0d8816435d2e94c1ea54a187b788b
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 23:14:47 +0500

    Featured cards

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991f1c5d879ed3e521ed7071551ad94fb2cd3ff5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f176dc99429977e9a74e28347366ece4dea8c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
