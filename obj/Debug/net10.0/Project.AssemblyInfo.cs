b1fb955f3962c20abcc16ff491c5d4b2a95c81af
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 10:01:21 +0500

    updating spinner

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4e388d42249d0ccd0f67c4c91dba9e86638499")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486d1b6a4af868643220413f8ddfe8449c44815c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
