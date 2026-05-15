bcbe387191d377dcc7c37fe2bd9b0fb43202de3c
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 10:59:34 +0500

    added url

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c911040b463a4bb951001bd754cc8b47dab92e5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aac97b8d5a31dee8fed497330222c5848994fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
