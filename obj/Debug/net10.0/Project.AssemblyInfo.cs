a78c652952402d4368313b77056f9239b56bd6fd
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 09:39:08 +0500

    added icon

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92f5396b5d6581c3930a037a002a4fc27dd5e4f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05e8018067a15dbd1265cf91ed53bd6e812dd3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
