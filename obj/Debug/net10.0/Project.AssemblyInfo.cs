b88650e5b502e578447f894d726be2500e8552bb
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-15 10:47:31 +0500

    updated slider

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8858666efb7ba6d7e6efbec6a0e79f7632e31eab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9abde99185816909916d35f2cdccdd58998ede73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
