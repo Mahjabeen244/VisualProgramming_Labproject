6088cf2407cbafcda1c0d69a5c8ddfe8b675df06
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-16 15:23:05 +0500

    removed error

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82b963f8c9b37c5449368f6539c47096e32db63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a6aa9cf8eab2cb853c15d48375e78c82f9241f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
