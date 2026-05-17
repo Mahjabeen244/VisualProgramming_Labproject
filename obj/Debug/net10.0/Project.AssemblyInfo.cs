deda24a7a313c70b6dec07834dfb26a5d80abd7d
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-17 08:47:49 +0500

    detail containers

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe01c1849bfdef5c269528e6de78b54ba67721c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78180bced0b359793c15f80e46aa398791a1166")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
