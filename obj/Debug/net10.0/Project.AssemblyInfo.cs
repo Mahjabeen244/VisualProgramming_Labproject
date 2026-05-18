224de38e5b56c77c74e255a77643bd31d1d42fff
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-18 12:13:11 +0500

    job page

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c255d8c6fc613ef11acda4569ac82eddf589ab77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bdc9beff08a746b04b595cd366d3c90e35ee724")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
