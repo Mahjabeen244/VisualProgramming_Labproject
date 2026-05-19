d914af2c7bd020a9ce77ac3ccbe508433e6c264b
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-19 14:58:30 +0500

    updated navlink

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78110e598f9130e1c684d77da30ce60adc486bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc2d556243d8209f23127d10a57a3d7716746fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
