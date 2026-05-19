7ee7a21e5644617fa6ce8b45c8aaddd8818f78a0
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-19 13:00:38 +0500

    added job filtering

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99cbd8f7182c90d0ac2f368092727f4172ab81d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f189e10a46e7a92094f4e223ff94bc24397adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
