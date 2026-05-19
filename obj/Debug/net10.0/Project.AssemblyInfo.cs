c1db1480624671a93cac0b9da6565ed90b202ea3
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-19 19:39:39 +0500

    added model departments

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181d2bb708e3c41f6ec9474256d8a4ec584814dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6580db7a287164aa61339653f6f90ef42926613a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
