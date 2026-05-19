181d2bb708e3c41f6ec9474256d8a4ec584814dd
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-19 19:10:48 +0500

    updating layout

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb72263f1ba8671b4306b245ef7b426871176555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13967fa79e5332714787627c19595e80633466b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
