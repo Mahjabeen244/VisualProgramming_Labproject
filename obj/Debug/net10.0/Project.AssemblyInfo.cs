d99cbd8f7182c90d0ac2f368092727f4172ab81d
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-19 12:48:59 +0500

    adding more job entries

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df7afd1276d2ad43135acaeba076e7845198afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8521b97a07fdeee613562fa0f58ce473edfd6cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
