a0b32e87fabb06c9234b8829abc812c29b95260b
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-20 16:49:11 +0500

    adding binding in @code

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc0d47e45a6c650273afa4985ee1dc4fd95002d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658633df3c007a53672d80dee5bed95c82aee3cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
