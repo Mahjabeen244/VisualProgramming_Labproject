9fc0d47e45a6c650273afa4985ee1dc4fd95002d
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-20 06:34:36 +0500

    added captcha box

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12444284a55a589055873d5002e6e1a3abb657d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f81e69ecd34775381d6897dd719e720660280ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
