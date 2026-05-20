62a920bfd485e19ec15552cc9a6969b02b42ead7
Mahjabeen244 <247266@students.au.edu.pk>
2026-05-20 17:00:21 +0500

    apply.razor page

--- obj/Debug/net10.0/Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d305ead63003057a9a9590c5dc75400ff03d5de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04469c1d18e974c899ca848acefd6fca486b8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
